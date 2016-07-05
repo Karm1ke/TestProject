8d4747e1f597560abf0a6c9c9268b0ff75bcddfb
mike <mihail6666@gmail.com>
2016-07-05 21:06:09 +0500

    поменял кнопки местами

--- Backuper/MainForm.Designer.cs
+++ Backuper/MainForm.Designer.cs
@@ -119,7 +119,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(21, 149);
+            this.button2.Location = new System.Drawing.Point(21, 116);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(257, 23);
             this.button2.TabIndex = 6;
@@ -139,7 +139,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(21, 120);
+            this.button1.Location = new System.Drawing.Point(21, 145);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(257, 23);
             this.button1.TabIndex = 4;
